069c62ec6f75197dc6598231d43dbe06151471ca
ihsanfrr <ihsan.fajardan@gmail.com>
2023-09-08 14:11:21 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,190 @@
+"use client"
 import Image from 'next/image'
 
+import logoBrand from './../../public/icons/logo-brand.png'
+import logoRaican from './../../public/icons/raican.png'
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const countdownDate = new Date('2023-10-09T00:00:00').getTime();
+
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    
+    const href = e.currentTarget.href;
+    const targetId = href.replace(/.*\#/, "");
+    
+    const elem = document.getElementById(targetId);
+    elem?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const calculateTimeLeft = () => {
+    const now = new Date().getTime();
+    const timeDifference = countdownDate - now;
+
+    if (timeDifference <= 0) {
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    return {
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+
+return (
+<main>
+  <nav className="bg-white border-b border-gray-200">
+    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="/" className="flex items-center">
+        <Image src={logoBrand} alt={''} height={50} className='mr-3' />
+        <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
+          DKC Subang
+        </span>
+      </a>
+      <div className="flex md:order-2">
+        <a href="https://www.facebook.com/Dkcsubang" target='_blank'>
+          <div className='ml-2 border-gray-400 border h-8 w-8 rounded-full flex items-center justify-center cursor-pointer hover:bg-[#EDB11E] hover:border-[#EDB11E] text-gray-400 hover:text-white'>
+            <i className="ri-facebook-fill"></i>
+          </div>
+        </a>
+        <a href="https://twitter.com/dkc_subang0913" target='_blank'>
+          <div className='ml-2 border-gray-400 border h-8 w-8 rounded-full flex items-center justify-center cursor-pointer hover:bg-[#EDB11E] hover:border-[#EDB11E] text-gray-400 hover:text-white'>
+            <i className="ri-twitter-x-fill"></i>
+          </div>
+        </a>
+        <a href="https://www.instagram.com/dkc_subang0913">
+          <div className='ml-2 border-gray-400 border h-8 w-8 rounded-full flex items-center justify-center cursor-pointer hover:bg-[#EDB11E] hover:border-[#EDB11E] text-gray-400 hover:text-white'>
+            <i className="ri-instagram-line"></i>
+          </div>
+        </a>
+      </div>
+    </div>
+  </nav>
+
+  {/* Section Hero */}
+  <section className="bg-white min-h-screen overflow-hidden">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl text-center flex flex-col lg:flex-row-reverse justify-around items-center h-full">
+    <div>
+      <Image src={logoRaican} alt={""} className='h-[377px] w-[201px] md:h-[377px] md:w-[201px] lg:h-[600px] lg:w-[321px]'/>
+    </div>
+    <div>
+      <div className="grid grid-flow-col gap-5 text-center justify-center auto-cols-max my-10">
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content border rounded text-black">
+          <span className="countdown text-5xl">
+            <span>{timeLeft.days}</span>
+          </span>
+          Hari
+        </div>
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content border rounded text-black">
+          <span className="countdown text-5xl">
+            <span>{timeLeft.hours}</span>
+          </span>
+          Jam
+        </div>
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content border rounded text-black">
+          <span className="countdown text-5xl">
+            <span>{timeLeft.minutes}</span>
+          </span>
+          Menit
+        </div>
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content border rounded text-black">
+          <span className="countdown text-5xl">
+            <span>{timeLeft.seconds}</span>
+          </span>
+          Detik
         </div>
       </div>
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
+        Raimuna Subang 2023
+      </h1>
+      <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
+        Buper Ranggawulung, 09-13 Oktober 2023
+      </p>
+      <a href="#required_files" onClick={handleScroll} 
+        className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-[#EDB11E] hover:bg-[#E2A712] focus:ring-4 focus:ring-[#F6D88E]">
+        Ikut Berpartisipasi
+        <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+          viewBox="0 0 14 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9" />
+        </svg>
+      </a>
+    </div>
+  </div>
+</section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+  {/* Section Required Files */}
+  <section className="bg-white max-h-full" id='required_files'>
+  <div className='flex flex-col items-center justify-center text-center py-8'>
+    <p className='p-0 mb-5 text-[#EDB11E]'>TATA CARA</p>
+    <h2 className="text-4xl font-bold mb-10">Berpartisipasi di Raimuna Subang</h2>
+    <div className="w-2/3 py-10 text-left">
+      <div className='inline text-4xl font-bold'>1. </div>
+      <div className='inline text-lg'>Pastikan untuk membaca dengan cermat petunjuk teknis dan pelaksanaan yang tersedia.</div>
+      <hr className='my-10' />
+      <div className='inline text-4xl font-bold'>2. </div>
+      <div className='inline text-lg'>Ada beberapa berkas yang perlu Anda persiapkan dengan seksama, berikut adalah template berkas nya yang bisa di <a href="https://linktr.ee/dkcsubang" className='text-[#EDB11E] underline' target='_blank'>Lihat</a></div>
+      <hr className='my-10' />
+      <div className='inline text-4xl font-bold'>3. </div>
+      <div className='inline text-lg'>Setelah Anda memastikan bahwa semua persiapan telah selesai, saatnya untuk melanjutkan dengan melakukan pengisian data yang diperlukan pada link berikut <a href="https://forms.gle/TThVm1Ettrqu7CrZ9" className='text-[#EDB11E] underline' target='_blank'>Google Form</a> </div>
+    </div>
+  </div>
+  </section>
+
+  <footer>
+    <div className='bg-black py-10 text-center'>
+      <div className="flex justify-center mb-10">
+        <a href="https://www.facebook.com/Dkcsubang" target='_blank'>
+          <div
+            className='ml-4 border-gray-400 border h-8 w-8 rounded-full flex items-center justify-center cursor-pointer hover:bg-[#EDB11E] hover:border-[#EDB11E] text-gray-400 hover:text-white'>
+            <i className="ri-facebook-fill"></i>
+          </div>
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
+        <a href="https://twitter.com/dkc_subang0913" target='_blank'>
+          <div
+            className='ml-4 border-gray-400 border h-8 w-8 rounded-full flex items-center justify-center cursor-pointer hover:bg-[#EDB11E] hover:border-[#EDB11E] text-gray-400 hover:text-white'>
+            <i className="ri-twitter-x-fill"></i>
+          </div>
         </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+        <a href="https://www.instagram.com/dkc_subang0913" target='_blank'>
+          <div
+            className='ml-4 border-gray-400 border h-8 w-8 rounded-full flex items-center justify-center cursor-pointer hover:bg-[#EDB11E] hover:border-[#EDB11E] text-gray-400 hover:text-white'>    
+            <i className="ri-instagram-line"></i>
+          </div>
         </a>
       </div>
-    </main>
-  )
+      <p className='text-gray-100 font-extralight'>© 2023 - Raimuna Subang Tahun 2023. by JhonyLandis</p>
+    </div>
+  </footer>
+</main>
+)
 }
